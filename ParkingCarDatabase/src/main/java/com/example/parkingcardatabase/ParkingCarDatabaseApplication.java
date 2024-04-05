512d0c8fc6ec76d349fccab5805b43978e1b51a6
krysi08 <145769907+krysi08@users.noreply.github.com>
2024-04-05 13:20:05 +0200

    Add files via upload

---
+++ ParkingCarDatabase/src/main/java/com/example/parkingcardatabase/ParkingCarDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingcardatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingCarDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingCarDatabaseApplication.class, args);
+    }
+
+}
