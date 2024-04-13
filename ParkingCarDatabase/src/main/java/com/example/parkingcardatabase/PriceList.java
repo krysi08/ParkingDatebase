57f9fd5e9ca2c6ff902d3254f4c1acd59cd29613
Naitsyrk95 <kry.zawadzki2203@gmail.com>
2024-04-13 14:20:55 +0200

    Add files via upload

--- ParkingCarDatabase/src/main/java/com/example/parkingcardatabase/PriceList.java
+++ ParkingCarDatabase/src/main/java/com/example/parkingcardatabase/PriceList.java
@@ -5,15 +5,18 @@ public class PriceList {
 
         PriceList list = new PriceList();
 
-        list.settleCostsParking(7);
+        list.settleCostsParking(96);
     }
 
     public void settleCostsParking(double timeParking) {
         if (timeParking >= 10.00 && timeParking <= 60.00) {
             System.out.println("Please pay off 5EUR");
-        } else if (timeParking > 60.00) {
+        } else if (timeParking > 60.00 && timeParking <= 240.00) {
             double valueCosts = timeParking * 5 / 60.00;
             System.out.println("You have to pay off" + " " + valueCosts + " " + "EUR");
+        } else if (timeParking > 240.00) {
+            double discountCosts = (timeParking * 4) / 60;
+            System.out.println("You have to pay off" + " " + discountCosts + " " + "EUR");
         } else {
             System.out.println("No fees");
         }
